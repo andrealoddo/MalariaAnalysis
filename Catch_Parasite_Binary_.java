ea4e05bb65c464b686f0107566dd0df68b739826
tatalessap <martapibirimp@gmail.com>
2018-05-22 11:32:48 +0200

    Add files via upload
    Aggiunta commenti

--- Catch_Parasite_Binary_.java
+++ Catch_Parasite_Binary_.java
@@ -12,16 +12,18 @@ import ij.plugin.Thresholder;
 import java.awt.*;
 
 /*Catch_Parasite by Tatalessap
-* Catch_Parasite è una classe volta alla misurazione di elementi all'interno di una immagine binaria.
-* Si appoggia al Plugin "ThresholdAdjuster" per poter individuare le zone e gli oggetti da analizzare.
-* Implementa Plugin ma al suo interno vi è una classe Parasite che estende a sua volta il PluginFilter "ParticleAnalyzer"
-* aggiungendo misure non presenti.*/
+ * Catch_Parasite è una classe volta alla misurazione di elementi all'interno di una immagine binaria.
+ * Si appoggia al Plugin "ThresholdAdjuster" per poter individuare le zone e gli oggetti da analizzare.
+ * Implementa Plugin ma al suo interno vi è una classe Parasite che estende a sua volta il PluginFilter "ParticleAnalyzer"
+ * aggiungendo misure non presenti.*/
 
 public class Catch_Parasite_Binary_ implements PlugIn {
     /*Variabili booleani corrispondenti alle misure implementate e aggiunte.*/
-    /*Sezione 1: ConvexHull*/
+    /*Sezione 1*/
+    /*ConvexHull*/
     boolean doConvexArea;
     boolean doConvexPerimeter;
+
     boolean doMINRMAXR;
     boolean doAspRatio;
     boolean doCirc;
@@ -38,37 +40,46 @@ public class Catch_Parasite_Binary_ implements PlugIn {
     boolean doModRatio;
     boolean doSphericity;
     boolean doElongation;
+    /*Sezione 2*/
     boolean doNormPeriIndex;
 
     /*piGreco necessario per alcuni calcoli*/
     double pigreco= 3.1415926535;
 
-    /**/
+    /*Metodo run necessaria per i PlugIn
+    * Memorizza l'immagine in ingresso
+    * richiama il metodo catch_parasite_running dandogli in ingresso l'immagine*/
     public void run(String arg) {
         ImagePlus imp = IJ.getImage();
         catch_parasite_running(imp); //metodo presente dopo
     }
 
-    /*Funzione per avviare e creare l'oggetto Parasite (classe interna)*/
+    /*Metodo catch_parasite_running che riceve in ingresso l'immagine
+    * creazione di un nuovo oggetto Parasite, implementato come classa interna
+    * avvio attraverso setup e controllo
+    * avvio attraverso run dell'oggetto Parasite
+    * visualizzazione risultati da super - super classe Analyzer*/
     public void catch_parasite_running (ImagePlus imp) {
         Parasite parasite = new Parasite();
         int flags = parasite.setup("", imp);
         /*Controllo*/
         if (flags == PlugInFilter.DONE)
-                return;
+            return;
         parasite.run(imp.getProcessor());
         /*visualizzazione dei risultati*/
         Analyzer.getResultsTable().show("Results");
     }
 
-
+    /*Metodo della creazione della finestra di dialogo, visualizzazione del:
+    * - checkbox per selezionare ogni misura
+    * - checkbox per selezionare misure singole*/
     public boolean genericDialog() {
         GenericDialog gd = new GenericDialog("Parassite Prova", IJ.getInstance());
         gd.addStringField("Title: ", "Catch parasite");
         gd.addMessage("Choise measures");
         gd.addMessage("The measures area, perim, feret, feret min are necessary for the PlugIn");
 
-        gd.addCheckbox("SelectAll", true);
+        gd.addCheckbox("SelectAll", true); //tutte le misure
 
         gd.addCheckbox("Convex Area", false);
         gd.addCheckbox("Convex Perimeter", false);
@@ -134,46 +145,29 @@ public class Catch_Parasite_Binary_ implements PlugIn {
             doElongation = gd.getNextBoolean();
             doNormPeriIndex = gd.getNextBoolean();
         }
-
         return true;
     }
 
-    //da sistemare
-    public void setImage(){
-        ThresholdAdjuster th = new ThresholdAdjuster();
-    }
-
-
-    //da Analyzer ma rivisitato dopo il confronto con i dati calcolati da MatLab
+    /*Riguardante ConvexHull-
+    * Rivisitazione del metodo getArea da Analyzer(super-super classe) per cui dati i punti del poligono calcola l'area
+    * trattandolo come se fosse composto da danti piccoli triangoli*/
     final double getArea(Polygon p) {
         if (p==null) return Double.NaN;
         int carea = 0;
         int iminus1;
-        /*FUNZIONAMENTO
-        * tratta il polygon in maniera tale da scomporlo in più triangoli.*/
-        /*ciclo originale
-        for (int i=0; i<p.npoints; i++) {
-            iminus1 = i-1;
-            if (iminus1<0) iminus1=p.npoints-1;
-            carea += (p.xpoints[i]+p.xpoints[iminus1])*(p.ypoints[i]-p.ypoints[iminus1]);
-        }*/
 
         for(int i =0; i<p.npoints-1;i++){
             iminus1=i-1;
             if(iminus1<0) iminus1=p.npoints-1;
             carea+=(p.xpoints[i]+p.xpoints[iminus1])*(p.ypoints[i]-p.ypoints[iminus1]);
         }
-
-        //lo triangolizza?
-        //return (double) p.npoints;
         return (Math.abs(carea/2.0));
     }
 
+    /*Riguardante ConvexHull-
+    * Calcolo del perimetro dato i punti del poligono e calcolando le distanze trai punti*/
     final double getPerimeter (Polygon p){
         if(p==null) return  Double.NaN;
-        /*FUNZIONAMENTO
-         * somma le distanze da un punto A ad un punto B
-         * */
 
         double cperimeter = 0.0;
         int iminus1;
@@ -190,32 +184,21 @@ public class Catch_Parasite_Binary_ implements PlugIn {
 
             cperimeter +=Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
         }
-
         return cperimeter;
     }
 
-    /*double [] getMinRMaxR(float xCenterOfMass, float yCenterOfMass){
-        double [] minR_maxR;
-
-        return minR_maxR;
-    }*/
-
-
-
-    //cuore
-    /*? una classe interna dichiarata nella classe Parassite_Plugin*/
-
+    /*Metodo addMeasure necessario per il calcolo delle misure aggiuntive a cui vengono passati i valori già trattati nella classe estesa (ParticleAnalyzer)*/
     void addMeasure(ImageStatistics stats, Roi roi, ResultsTable rt, ImagePlus imp){
         ImageProcessor ip = imp.getProcessor();
-        ip.setRoi(roi);
-        float[] areas = rt.getColumn(ResultsTable.AREA);
-        float[] ferets = rt.getColumn(ResultsTable.FERET);
+        ip.setRoi(roi); //settaggio della zona di interesse
+        float[] areas = rt.getColumn(ResultsTable.AREA); //recupero di alcuni valori necessari per poter calcolare le nuove misure
+        float[] ferets = rt.getColumn(ResultsTable.FERET); //pescati dalle colonne perchè non risiedono in nessuna variabile richiamabile
         float[] breadths =  rt.getColumn(ResultsTable.MIN_FERET);
         float[] perims = rt.getColumn(ResultsTable.PERIMETER);
-        //double convexArea = roi!=null?getArea(roi.getConvexHull()):stats.pixelCount;
+        //Riguardante ConvexHull: trattamento di un oggetto Polygon (Riguardante la ROI data)
         double convexArea = getArea(roi.getConvexHull());
         double convexPerimeter = getPerimeter(roi.getConvexHull());
-        //double peri = roi!=null?getPerimeter(roi.getConvexHull()):stats.pixelCount;
+        //ciclo per l'aggiunta dei nuovi valori
         for (int i = 0; i < areas.length; i++) {
             // double [] minR_maxR = getMinRMaxR(stats.xCenterOfMass, stats.yCenterOfMass);
             if(doConvexArea) //Area of the convex hull polygon
@@ -266,30 +249,32 @@ public class Catch_Parasite_Binary_ implements PlugIn {
                 boundaries.
                 The normalized perimeter index uses the equal area circle to normalize the metric.
                 */
-
         }
             /*float[] meanRadius= rt.getColumn(ResultsTable.MEAN);
             float[] standardDeviationOfRadii= rt.getColumn(ResultsTable.STD_DEV);
             if(doHaralickRatio) DA FARE SU PIù OGGE
                 // rt.addValue("*Haralick ratio", meanRadius[i]/standardDeviationOfRadii[i]); //da rivedere
                 */
-
     }
 
+    /*Inner class Parasite che estende la classe ParticleAnalyzer
+    * Al suo interno, attraverso gli Override, si estendono i meotdi già presenti nella classe ParticleAnalyzer*/
     class Parasite extends ParticleAnalyzer {
+        //Salvataggio delle misure già settate in SetMeasurements
         int old_measures = Analyzer.getMeasurements();
+        //Salvataggio delle nuove misure necessarie per il calcolo delle nuove
         int necessary_measures =
                 ALL_STATS+SHAPE_DESCRIPTORS
-                -KURTOSIS
-                -NaN_EMPTY_CELLS
-                -INVERT_Y
-                -INTEGRATED_DENSITY
+                        -SKEWNESS
+                        -STD_DEV
+                        -KURTOSIS
+                        -NaN_EMPTY_CELLS
+                        -INVERT_Y
+                        -INTEGRATED_DENSITY
                 ;
-
-
+        /**/
         @Override
         public boolean showDialog(){
-
             boolean flag = super.showDialog();
             super.staticShowChoice = 1; //*protected static final int NOTHING=0, OUTLINES=1, BARE_OUTLINES=2, ELLIPSES=3, MASKS=4, ROI_MASKS=5,
             //                             OVERLAY_OUTLINES=6, OVERLAY_MASKS=7;*//
@@ -298,7 +283,7 @@ public class Catch_Parasite_Binary_ implements PlugIn {
             return genericDialog();
         }
 
-        //SAVE RESULT
+        /**/
         @Override
         protected void saveResults(ImageStatistics stats, Roi roi) {
             //ThresholdAdjuster th = new ThresholdAdjuster();
